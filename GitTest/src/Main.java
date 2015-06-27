2778dd53fdcfe3d85d16ac98b46c50e815309647
Rostyslav Viner <vkilgara@gmail.com>
2015-06-27 13:46:57 +0300

    # 3 for master

--- GitTest/src/Main.java
+++ GitTest/src/Main.java
@@ -2,11 +2,7 @@
  * Created by root on 27.06.2015.
  */
 public class Main {
-    public void sraka () {
-        return;
-    }
-
     public static void main (String[] args) {
-        System.out.println("New");
+        System.out.println("Master");
     }
 }
